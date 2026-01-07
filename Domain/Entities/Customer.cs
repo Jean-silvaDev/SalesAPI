401d80c1b148abba89412db86499cab0674cc8d5
Jean-silvaDev <franciscojean053@gmail.com>
2026-01-07 20:04:18 -0300

    #Feature: Create Product and Customer entities.

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string? CPF { get; set; }
+    public DateTime InsertAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public ProductType? Type { get; set; }
+        public DateTime InsertAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
